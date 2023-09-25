6c812d65b1e099184648e8c3dc2c7d28443ad7ee
cooligus <pewienjaaa@gmail.com>
2023-09-25 09:37:44 +0200

    Komit jakby co

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BibleComponent } from './bible/bible.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: BibleComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BibleComponent } from './bible/bible.component';
+import { SkeletonModule } from 'primeng/skeleton';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, BibleComponent],
+  imports: [BrowserModule, AppRoutingModule, SkeletonModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
